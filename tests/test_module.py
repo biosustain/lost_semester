ae94e039e1708085e86106f1415987b7f1fe8ea0
carrascomj <carrascomurielj@gmail.com>
2022-10-17 20:16:55 +0200

    python: unit test with proper name

--- tests/test_module.py
+++ tests/test_module.py
@@ -3,7 +3,11 @@
 from mypackage.module import hello
 
 
-def test_hello():
+def test_hello_is_none():
     """Test that hello actually does not return anything."""
     res = hello()
     assert res is None
+
+
+def test_hello_with_fixtures(just_none):
+    assert hello() is just_none
