16a9a14a5a5aa1ac7a28b82b5dcb021efb54747d
carrascomj <carrascomurielj@gmail.com>
2022-10-17 10:38:20 +0200

    nvim: edit LATEX with texlab

--- nvim/lua/mylsp.lua
+++ nvim/lua/mylsp.lua
@@ -1,5 +1,24 @@
--- LSP configs
+-- python
 require("lspconfig").pylsp.setup({
   cmd = {"pylsp"},
   filetypes = { "python" },
 })
+
+-- latex
+require("lspconfig").texlab.setup({
+  cmd = { "texlab" },
+  settings = {
+    texlab = {
+      build = {
+        onSave = true;
+      },
+      forwardSearch = {
+        -- this should be your PDF viewer executable
+        executable = "zathura",
+        args = {"--synctex-forward", "%l:1:%f", "%p"},
+        onSave = true;
+      }
+    }
+  }
+})
+
